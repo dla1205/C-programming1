dd589e2db11919df79cd0e6be9ff0bdb3639095d
dla1205 <lej01090109@gmail.com>
2026-06-02 17:03:56 +0900

    Add files via upload

---
+++ 6월2일 실습 4번.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+    int arr[3][3];
+    int value = 1;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            arr[i][j] = value++;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
